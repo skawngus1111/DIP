936716a01dfce4caba42aa78feb4ba486ff2ac2c
Ju-Hyeon Nam <skawngus1111@gmail.com>
2021-03-13 21:52:52 +0900

    chapter3 histogram matching revision

--- ch3/test.m
+++ ch3/test.m
@@ -3,38 +3,39 @@ addpath('./ch3')
 addpath('./ch3/utils')
 
 minValue=1; maxValue=256;
-I = imread('./example/Einstein.jpg');
+I = imread('./example/low_cont.jpg');
+% I = rgb2gray(I);
 
-figure(1); 
-subplot(1, 2, 1); imshow(I); title('input image');
-subplot(1, 2,2); bar(im2hist(I, minValue, maxValue)); title('histogram of input image');
+% figure(1); 
+% subplot(1, 3, 1); imshow(I); title('input image');
+% subplot(1, 3, 2); bar(im2hist(I, minValue, maxValue)); title('custom histogram of input image');
+% subplot(1, 3, 3); imhist(I); title('MATLAB histogram of input image');
 
-% I = grayslice(I, 2^3);
-[equalizedImage, cdf] = histequal(I, minValue, maxValue);
+[equalizedImage, T] = histequal(I, minValue, maxValue);
 
 I_eq = histeq(I);
 
-figure(2);
-subplot(3, 3, 1); imshow(I); title("input image");
-subplot(3, 3, 2); imshow(equalizedImage); title("custom");
-subplot(3, 3, 3); imshow(I_eq); title("matlab");
-axis tight; axis off
-
-subplot(3, 3, 4); bar(im2hist(I, minValue, maxValue)); xlim([minValue, maxValue]);
-subplot(3, 3, 5); bar(im2hist(equalizedImage, minValue, maxValue)); xlim([minValue, maxValue]);
-subplot(3, 3, 6); bar(im2hist(I_eq, minValue, maxValue)); xlim([minValue, maxValue]);
-
-subplot(3, 3, [7, 8, 9]); 
-plot(cdf, 'r', 'LineWidth', 2); 
-xlim([minValue, maxValue]); ylim([minValue, maxValue]);
-title("image transform function, s=T(r)");
+% figure(2);
+% subplot(3, 3, 1); imshow(I); title("input image");
+% subplot(3, 3, 2); imshow(equalizedImage); title("custom");
+% subplot(3, 3, 3); imshow(I_eq); title("matlab");
+% axis tight; axis off
+% 
+% subplot(3, 3, 4); bar(im2hist(I, minValue, maxValue)); xlim([minValue, maxValue]);
+% subplot(3, 3, 5); bar(im2hist(equalizedImage, minValue, maxValue)); xlim([minValue, maxValue]);
+% subplot(3, 3, 6); bar(im2hist(I_eq, minValue, maxValue)); xlim([minValue, maxValue]);
+% 
+% subplot(3, 3, [7, 8, 9]); 
+% plot(T, 'r', 'LineWidth', 2); 
+% xlim([minValue, maxValue]); ylim([minValue, maxValue]);
+% title("image transform function, s=T(r)");
 
 SourceImage = imread('./example/low_cont.jpg'); % source image
 TargetImage = imread('./example/high_cont.jpg'); % target image
 
 TargetImage = rgb2gray(TargetImage);
 
-[MatchedImage, InverseTargetCDF, TargetCDF] = histMatching(SourceImage, TargetImage, minValue, maxValue);
+[MatchedImage, GInverse, G] = histMatching(SourceImage, TargetImage, minValue, maxValue);
 matlabCode = imhistmatch(SourceImage, TargetImage);
 
 figure(3);
@@ -49,8 +50,8 @@ subplot(3, 4, 7); bar(im2hist(MatchedImage, minValue, maxValue)); xlim([minValue
 subplot(3, 4, 8); bar(im2hist(matlabCode, minValue, maxValue)); xlim([minValue, maxValue]);
 
 subplot(3, 4, [9, 10, 11, 12]); 
-plot(TargetCDF, 'r', 'LineWidth', 2); hold on;
-plot(InverseTargetCDF, 'r:', 'LineWidth', 2);
+plot(G, 'r', 'LineWidth', 2); hold on;
+plot(GInverse, 'r:', 'LineWidth', 2);
 plot([minValue, maxValue], [minValue, maxValue], 'k:', 'LineWidth', 2);
 xlim([minValue, maxValue]); ylim([minValue, maxValue]);
 title("image transform function"); legend("$G(z)$", "$G^{-1}(s)$", "$y=x$", 'Interpreter','latex')
--- ch3/utils/histMatching.m
+++ ch3/utils/histMatching.m
@@ -1,4 +1,4 @@
-function [MatchedImage, InverseTargetCDF, TargetCDF] = histMatching(SourceImage, TargetImage, minValue, maxValue)
+function [MatchedImage, GInverse, G] = histMatching(SourceImage, TargetImage, minValue, maxValue)
 %histMatching : sourceImage를 targetImage의 히스토그램으로 매칭시키는 함수
 %   input 
 %       1. sourceImage : 매칭시킬 source 영상
@@ -7,44 +7,34 @@ function [MatchedImage, InverseTargetCDF, TargetCDF] = histMatching(SourceImage,
 
 % Basic Algorithm
 %   1. source 영상을 히스토그램 평활화적용
-%   2. target 영상의 히스토그램 계산
-%   3. target 영상에서 평활화된 source 영상의 히스토그램을 갖기 위한 함수 G 계산
+%   2. target 영상에서 평활화된 source 영상의 히스토그램을 갖기 위한 함수 G 계산
 %       G(z) = \sum_{j=1}^{k}p_{z}(z_{j})
-%   4. G(z) 반올림
-%   5. 모호성 문제(ambiguous problem)를 방지하기 위해서 s_{k}의 값 중 가장 가까운 G(z_{q}) 찾기
-%   6. STEP 5에서 찾은 s_{k}와 G(z_{k})의 쌍을 이용하여 s_{k} -> z_{q}로 매핑
+%   3. 모호성 문제(ambiguous problem)를 방지하기 위해서 s_{k}의 값 중 가장 가까운 G(z_{q}) 찾기
+%   4. STEP 3에서 찾은 s_{k}와 G(z_{k})의 쌍을 이용하여 s_{k} -> z_{q}로 매핑
 
-% STEP 1. source 영상을 히스토그램 평활화적용
-[EqualizedSource, SourceCDF] = histequal(SourceImage, minValue, maxValue);
+% STEP 1. source 영상(r)을 히스토그램 평활화 적용(s) : r -> s
+[~, T] = histequal(SourceImage, minValue, maxValue);
 
-% STEP 2. target 영상의 히스토그램 계산
-[width, height] = size(TargetImage); % 입력 영상의 크기
-totalPixel = width * height; % 전체 픽셀의 개수
-TargetHistogram = im2hist(TargetImage, minValue, maxValue)/totalPixel;% 히스토그램 정규화(0 ~ 1)
+% STEP 2. target 영상에서 평활화된 source 영상의 히스토그램을 갖기 위한 함수 G 계산
+[~, G] = histequal(TargetImage, minValue, maxValue);
 
-% STEP 3. target 영상에서 평활화된 source 영상의 히스토그램을 갖기 위한 함수 G 계산
-TargetCDF = zeros(1, maxValue); % 입력 영상 히스토그램의 CDF를 저장할 변수
-TargetCDF(1) = TargetHistogram(1);
-for i=minValue+1:maxValue % 최솟값 ~ 최댓값까지 각 값에 대한 CDF 계산
-    TargetCDF(i) = TargetCDF(i-1) + TargetHistogram(i);
+% STEP 3. 모호성 문제를 방지하기 위해서 s_{k}의 값 중 가장 가까운 G(z_{q}) 찾기
+GInverse = zeros(1, maxValue);
+
+for i=1:maxValue
+    diff = abs(T(i) - G);
+    [~, idx] = min(diff);
+    GInverse(i) = idx - 1;
 end
 
-% STEP 4. G(z) 반올림
-TargetCDF = round((maxValue - 1)*TargetCDF);
 
-% STEP 5. 모호성 문제를 방지하기 위해서 s_{k}의 값 중 가장 가까운 G(z_{q}) 찾기
-InverseTargetCDF = zeros(1, maxValue);
-for i=minValue:maxValue
-    [~, idx] = min(abs(TargetCDF - SourceCDF(i)));
-    InverseTargetCDF(i) = idx - 1;
-end
+% STEP 4. STEP 3에서 찾은 s_{k}와 G(z_{k})의 쌍을 이용하여 s_{k} -> z_{q}로 매핑
+MatchedImage = zeros([size(SourceImage, 1), size(SourceImage, 2)], 'uint8');
 
-% STEP 6. STEP 5에서 찾은 s_{k}와 G(z_{k})의 쌍을 이용하여 s_{k} -> z_{q}로 매핑
-MatchedImage = EqualizedSource;
-
-for k=minValue:maxValue
-    idx = MatchedImage==(k-1);
-    MatchedImage(idx) = InverseTargetCDF(k);
+for i=1:size(SourceImage, 1)
+    for j=1:size(SourceImage, 2)
+        MatchedImage(i, j) = GInverse(SourceImage(i, j) + 1);
+    end
 end
 
 end
